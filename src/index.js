35f9848d1a7e64ee2d0397cfffc90f5f194f6e9b
luisherrera98 <luchoherrerafernandez@gmail.com>
2023-08-23 10:51:14 -0300

    config docker

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware para parsear JSON en las peticiones
+app.use(express.json());
+
+// Ruta de ejemplo
+app.get('/', (req, res) => {
+  res.send('¡Hola, esta es tu API Express!');
+});
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal en el servidor.');
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor en funcionamiento en http://localhost:${port}`);
+});
